f31a94b3507c4bc5d5a5f39e072068325bdab14e
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-12-08 11:49:01 +0300

    done

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+  let reversedWord = word.split('').reverse().join('');
+
+  if (word === reversedWord) {
+    return true;
+  } else if (word != reversedWord) {
+    return false;
+  }
+  
 }
+console.log(isPalindrome('Dog')) //debugger
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here ( to whoever is marking: I dont understand what's this for, kindly explain)
 */
 
 /*
   Add written explanation of your solution here
 */
+// I created a function which took in a parameter 'Word'. I then defined a variable 'reveredWord' to which
+// I assigned it to the reverse of the parameter 'Word". If both are simialar, then the concole should log 
+// true, hence if not similar ti should log false.
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
